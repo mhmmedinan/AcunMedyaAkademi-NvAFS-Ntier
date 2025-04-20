135898582ebb4e267a598bb2c944887d194456d4
mhmmedinan <minan.65@hotmail.com>
2025-04-20 12:08:51 +0300

    Repository Pattern Implementasyonu

---
+++ Entities/Model.cs
@@ -0,0 +1,27 @@
+﻿using Core.Entities;
+
+namespace Entities;
+
+public class Model:BaseEntity<Guid>
+{
+    public Guid BrandId { get; set; }
+    public string Name { get; set; }
+
+    public virtual Brand? Brand { get; set; } //ManyToOne
+
+
+    public virtual ICollection<Car> Cars { get; set; } // OneToMany ilişkiye sahip
+
+
+    public Model()
+    {
+        Cars = new HashSet<Car>();
+    }
+
+    public Model(Guid id,Guid brandId, string name)
+    {
+        Id = id;
+        BrandId = brandId;
+        Name = name;
+    }
+}
