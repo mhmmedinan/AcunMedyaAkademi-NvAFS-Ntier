7011c1edb530c1b8830c6a6d3745850e7e93908b
mhmmedinan <minan.65@hotmail.com>
2025-04-27 11:07:12 +0300

    entityconfiguration sınıfları ve migration yapıları oluşturuldu

--- Repositories/Concretes/EntityFramework/Contexts/BaseDbContext.cs
+++ Repositories/Concretes/EntityFramework/Contexts/BaseDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using System.Reflection;
 
 namespace Repositories.Concretes.EntityFramework.Contexts;
 
@@ -13,8 +14,18 @@ public class BaseDbContext:DbContext
     public DbSet<Car> Cars { get; set; }
 
 
-    public BaseDbContext(DbContextOptions dbContextOptions,IConfiguration configuration):base(dbContextOptions)
+    public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
     {
         Configuration = configuration;
+    }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        foreach(var relationShip in modelBuilder.Model.GetEntityTypes().SelectMany(x => x.GetForeignKeys()))
+        {
+            relationShip.DeleteBehavior = DeleteBehavior.Cascade;
+        }
     }
 }
